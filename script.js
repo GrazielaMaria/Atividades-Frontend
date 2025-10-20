67d64c7e870eb117d853397fb7dfbd819e39c42a
GrazielaMaria <graziela.maria324@gmail.com>
2025-10-20 15:27:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+functioneverificarcomida()
+let input=document.getElementById("comida").functioneverificarcomida
+Value.tolowercase()
+let paragrafo=documento.getElementoById("mesangem");
+if(input==="pao com ovo"){
+  paragrafo.textcontent="isso e cafe da manha";
+}
+else if(input==="feijao com arroz"){
+   paragrafo.textcontent="isso e almoco";
+}
+else{
+paragrafo.textcontent="isso e jantar"
+}
+
+
+
+
+ 
+    
+
+
+
+
+
+
+
+
+
+
+     
+
+
+     
+
+
+
+    
+
+
+
