2c38f2b079d3a4e81f9c0d7fafb34cc3e722a663
GrazielaMaria <graziela.maria324@gmail.com>
2025-10-31 20:35:29 -0300

    Add files via upload

---
+++ aula/src/components/Content.jsx
@@ -0,0 +1,9 @@
+export function Content() {
+    return (
+        <main>
+            
+            <img src="https://tse2.mm.bing.net/th/id/OIP.8TrfxJyW6X3qUHVuhXvsXgHaEK?pid=Api&P=0&h=180" alt="" />
+            <p>This is where the main content will be displayed.</p>
+        </main>
+    );
+}
